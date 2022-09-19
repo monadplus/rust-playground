fef784fa0a283397d3bf14f79fd204dde9c24d95
Arnau Abella <arnauabella@gmail.com>
2022-09-19 08:58:15 +0200

    async-stream test

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
+pub mod box_dropping;
 pub mod experiment_1;
 pub mod experiment_2;
 pub mod experiment_3;
 pub mod experiment_4;
 pub mod sequence_traverse;
 pub mod unsafe_rust;
-pub mod box_dropping;
+pub mod tokio_stream_test;
---
+++ src/tokio_stream_test.rs
@@ -0,0 +1,48 @@
+use std::{net::SocketAddr, io};
+
+use async_stream::{ stream, try_stream };
+use tokio::{pin, net::{TcpStream, TcpListener}};
+use tokio_stream::{Stream, StreamExt};
+
+fn zero_to_three() -> impl Stream<Item = u32> {
+    stream! {
+        for i in 0..3 {
+            yield i;
+        }
+    }
+}
+
+fn double<S: Stream<Item = u32>>(input: S)
+    -> impl Stream<Item = u32>
+{
+    stream! {
+        for await value in input {
+            yield value * 2;
+        }
+    }
+}
+
+fn bind_and_accept(addr: SocketAddr) -> impl Stream<Item = io::Result<TcpStream>>
+{
+    try_stream! {
+        let listener = TcpListener::bind(addr).await?;
+
+        loop {
+            let (stream, addr) = listener.accept().await?;
+            println!("received on {:?}", addr);
+            yield stream;
+        }
+    }
+}
+
+
+#[tokio::test]
+async fn test_async_stream() {
+    pin! {
+        let s = double(zero_to_three());
+    }
+
+    while let Some(value) = s.next().await {
+        println!("got {}", value);
+    }
+}
