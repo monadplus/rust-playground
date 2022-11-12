692ef23021d1222a39ec86e160cdfbaa3b86509a
Arnau Abella <arnauabella@gmail.com>
2022-11-12 17:04:08 +0100

    tracing
    More tracing
    
    Move tracing example to tracing.rs
    
    Asdf

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,6 @@ pub mod experiment_2;
 pub mod experiment_3;
 pub mod experiment_4;
 pub mod sequence_traverse;
-pub mod unsafe_rust;
 pub mod tokio_stream_test;
+pub mod unsafe_rust;
+pub mod tracing;
--- src/tokio_stream_test.rs
+++ src/tokio_stream_test.rs
@@ -1,48 +1,51 @@
-use std::{net::SocketAddr, io};
+#[cfg(test)]
+mod tests {
+    use std::{io, net::SocketAddr};
 
-use async_stream::{ stream, try_stream };
-use tokio::{pin, net::{TcpStream, TcpListener}};
-use tokio_stream::{Stream, StreamExt};
+    use async_stream::{stream, try_stream};
+    use tokio::{
+        net::{TcpListener, TcpStream},
+        pin,
+    };
+    use tokio_stream::{Stream, StreamExt};
 
-fn zero_to_three() -> impl Stream<Item = u32> {
-    stream! {
-        for i in 0..3 {
-            yield i;
+    fn zero_to_three() -> impl Stream<Item = u32> {
+        stream! {
+            for i in 0..3 {
+                yield i;
+            }
         }
     }
-}
 
-fn double<S: Stream<Item = u32>>(input: S)
-    -> impl Stream<Item = u32>
-{
-    stream! {
-        for await value in input {
-            yield value * 2;
+    fn double<S: Stream<Item = u32>>(input: S) -> impl Stream<Item = u32> {
+        stream! {
+            for await value in input {
+                yield value * 2;
+            }
         }
     }
-}
 
-fn bind_and_accept(addr: SocketAddr) -> impl Stream<Item = io::Result<TcpStream>>
-{
-    try_stream! {
-        let listener = TcpListener::bind(addr).await?;
+    #[allow(dead_code)]
+    fn bind_and_accept(addr: SocketAddr) -> impl Stream<Item = io::Result<TcpStream>> {
+        try_stream! {
+            let listener = TcpListener::bind(addr).await?;
 
-        loop {
-            let (stream, addr) = listener.accept().await?;
-            println!("received on {:?}", addr);
-            yield stream;
+            loop {
+                let (stream, addr) = listener.accept().await?;
+                println!("received on {:?}", addr);
+                yield stream;
+            }
         }
     }
-}
 
+    #[tokio::test]
+    async fn test_async_stream() {
+        pin! {
+            let s = double(zero_to_three());
+        }
 
-#[tokio::test]
-async fn test_async_stream() {
-    pin! {
-        let s = double(zero_to_three());
-    }
-
-    while let Some(value) = s.next().await {
-        println!("got {}", value);
+        while let Some(value) = s.next().await {
+            println!("got {}", value);
+        }
     }
 }
