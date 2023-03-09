7f53411eef91a94b9371a443177a3c9e83007644
Arnau Abella <arnauabella@gmail.com>
2023-03-09 10:12:36 +0100

    signal_hook_tokio

--- src/signal_hook.rs
+++ src/signal_hook.rs
@@ -32,3 +32,58 @@ mod tests {
         Ok(())
     }
 }
+
+// #[cfg(test)]
+mod tests_async {
+    use std::{process, time::Duration};
+
+    use signal_hook::consts::signal::*;
+    use signal_hook_tokio::Signals;
+
+    use futures::stream::StreamExt;
+    use std::process::Command;
+
+    async fn handle_signals(mut signals: Signals) {
+        while let Some(signal) = signals.next().await {
+            match signal {
+                SIGHUP => {
+                    // Reload configuration
+                    // Reopen the log file
+                }
+                SIGTERM | SIGINT | SIGQUIT => {
+                    // Shutdown the system;
+                }
+                _ => unreachable!(),
+            }
+            println!("Received signal: {signal:?}");
+        }
+    }
+
+    async fn main_thread() -> Result<(), Box<dyn std::error::Error>> {
+        for i in 1..=20 {
+            if i % 10 == 0 {
+                Command::new("kill")
+                    .args(["-s", "TERM", &process::id().to_string()])
+                    .spawn()?;
+            }
+            println!("Sleeping for 100ms..");
+            tokio::time::sleep(Duration::from_millis(100)).await;
+        }
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn signal_hook_tokio_test() -> Result<(), Box<dyn std::error::Error>> {
+        let signals = Signals::new(&[SIGHUP, SIGTERM, SIGINT, SIGQUIT])?;
+        let handle = signals.handle();
+
+        let signals_task = tokio::spawn(handle_signals(signals));
+
+        main_thread().await?;
+
+        handle.close();
+        signals_task.await?;
+
+        Ok(())
+    }
+}
